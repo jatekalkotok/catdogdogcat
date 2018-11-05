e1d109174c3194b267256b17a2c28e3e4eb0391b
ezisazis <levente.jonas@gmail.com>
2018-11-05 08:48:54 +0100

    Reformatting to standards

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import pygame
 
+
 def main():
     pygame.init()
     logo = pygame.image.load("logo32x32.png")
     pygame.display.set_icon(logo)
     pygame.display.set_caption("minimal program")
 
-    screen = pygame.display.set_mode((800,600))
+    screen = pygame.display.set_mode((800, 600))
     image = pygame.image.load("dog.png")
     [dog_x, dog_y] = image.get_size()
 
     background = pygame.Surface(screen.get_size())
-    background.fill((255,0,0))
+    background.fill((255, 0, 0))
     background = background.convert()
-    screen.blit(background, (0,0))
+    screen.blit(background, (0, 0))
 
     clock = pygame.time.Clock()
 
@@ -25,7 +26,7 @@ def main():
     [screen_x, screen_y] = screen.get_size()
     step = 10
 
-    x = screen_x/2 - dog_x/2
+    x = screen_x / 2 - dog_x / 2
     y = screen_y - dog_y
 
     while running:
@@ -40,18 +41,20 @@ def main():
                 if event.key == pygame.K_ESCAPE:
                     running = False
                 if event.key == pygame.K_LEFT:
-                    x = 0 if x <= 0 else x-step
+                    x = 0 if x <= 0 else x - step
                 if event.key == pygame.K_RIGHT:
-                    x = screen_x - dog_x if x >= screen_x - dog_x else x+step
+                    x = screen_x - dog_x if x >= screen_x - dog_x else x + step
 
         # refreshing shit
-        text = "FPS: {0:.2f}  Playtime: {1:.2f}".format(clock.get_fps(), playtime)
+        text = "FPS: {0:.2f}  Playtime: {1:.2f}".format(clock.get_fps(),
+                                                        playtime)
 
         screen.blit(background, (0, 0))
         screen.blit(image, (x, y))
 
         pygame.display.set_caption(text)
         pygame.display.update()
+
 
 if __name__ == "__main__":
     main()
