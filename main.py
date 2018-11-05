6c8e504475a7e74027c0f512efdb2ebe8a9768a9
ezisazis <levente.jonas@gmail.com>
2018-11-05 19:12:05 +0100

    Add cat as second "player"

--- main.py
+++ main.py
@@ -8,8 +8,11 @@ def main():
     pygame.display.set_caption("minimal program")
 
     screen = pygame.display.set_mode((800, 600))
-    image = pygame.image.load("dog.png")
-    [dog_x, dog_y] = image.get_size()
+    dog = pygame.image.load("dog.png")
+    cat = pygame.image.load("cat.png")
+
+    [dog_x, dog_y] = dog.get_size()
+    [cat_x, cat_y] = cat.get_size()
 
     background = pygame.Surface(screen.get_size())
     background.fill((255, 0, 0))
@@ -26,8 +29,11 @@ def main():
     [screen_x, screen_y] = screen.get_size()
     step = 10
 
-    x = screen_x / 2 - dog_x / 2
-    y = screen_y - dog_y
+    dogX = screen_x / 2 - dog_x / 2
+    dogY = screen_y - dog_y
+
+    catX = screen_x / 2 - cat_x / 2
+    catY = screen_y - cat_y
 
     while running:
         milliseconds = clock.tick(FPS)
@@ -41,16 +47,23 @@ def main():
                 if event.key == pygame.K_ESCAPE:
                     running = False
                 if event.key == pygame.K_LEFT:
-                    x = 0 if x <= 0 else x - step
+                    dogX = 0 if dogX <= 0 else dogX - step
+                if event.key == pygame.K_a:
+                    catX = 0 if catX <= 0 else catX - step
                 if event.key == pygame.K_RIGHT:
-                    x = screen_x - dog_x if x >= screen_x - dog_x else x + step
+                    dogX = screen_x - dog_x if dogX >= screen_x - dog_x \
+                        else dogX + step
+                if event.key == pygame.K_d:
+                    catX = screen_x - cat_x if catX >= screen_x - cat_x \
+                        else catX + step
 
         # refreshing shit
         text = "FPS: {0:.2f}  Playtime: {1:.2f}".format(clock.get_fps(),
                                                         playtime)
 
         screen.blit(background, (0, 0))
-        screen.blit(image, (x, y))
+        screen.blit(dog, (dogX, dogY))
+        screen.blit(cat, (catX, catY))
 
         pygame.display.set_caption(text)
         pygame.display.update()
