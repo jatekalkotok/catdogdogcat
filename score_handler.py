59cb41adbce665e477915a0877011d2777ec8d75
Siôn le Roux <sinisterstuf@gmail.com>
2018-11-25 05:07:46 +0100

    Extract high score filename and max limit to properties
    Also using with you don't need to close the file in read_scores either.

--- score_handler.py
+++ score_handler.py
@@ -1,22 +1,23 @@
 class ScoreHandler:
 
+    _file = "highscore.txt"
+    _max = 5 # Top 5! (can't really fit more on the screen)
+
     @staticmethod
     def read_scores():
-        file = open("highscore.txt", "r")
-        scores = file.readlines()
-        file.close()
-        return scores
+        with open(ScoreHandler._file, 'r') as f:
+            return f.readlines()
 
     @staticmethod
     def write_score(user, score):
-        with open("highscore.txt", 'r+') as f:
+        with open(ScoreHandler._file, 'r+') as f:
             scores = [s.split(':') for s in f.read().splitlines()]
             scores.append([user, score])
             scores = sorted(
                 scores,
                 key=lambda score: float(score[1]),
                 reverse=True
-            )[:5] # Top 5! (can't really fit more on the screen)
+            )[:ScoreHandler._max]
             f.seek(0)
             f.write("\n".join([ ':'.join(s) for s in scores ]) + "\n")
             f.truncate()
